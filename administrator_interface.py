6a03512ded78cbdc6aaf2b9ab7061d237e9f49d6
kalynkoyanagi <kalynk@uoregon.edu>
2022-02-23 22:29:20 -0800

    test commit

--- administrator_interface.py
+++ administrator_interface.py
@@ -3,7 +3,8 @@
 Title:	Administrator Interface for the Freedge Tracker System
 ===============================================================================
 Description:	TODO
-Authors:        TODO
+Authors:        Kalyn Koyanagi
 Last Edited:    2-22-2022
 Last Edit By:   Madison Werries
 """
+from tkinter import *
