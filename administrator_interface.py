4a1abb520d561a252de073e8efacfcfccfa93f0c
kalynkoyanagi <kalynk@uoregon.edu>
2022-02-23 23:06:09 -0800

    Very beginning of GUI stuff

--- administrator_interface.py
+++ administrator_interface.py
@@ -7,4 +7,60 @@ Authors:        Kalyn Koyanagi
 Last Edited:    2-22-2022
 Last Edit By:   Madison Werries
 """
+import sys
 from tkinter import *
+from tkinter import filedialog
+
+
+class admin_interface:
+    def __init__(self):
+        pass
+
+    def NewDatabase(self, event=None):
+        filename = filedialog.askopenfilename()
+        # TODO how can i get the db_path?
+        pass
+
+    def UpdateDatabase(self, event=None):
+        filename = filedialog.askopenfilename()
+        # TODO how can i get the db_path?
+        pass
+
+    def exit_(self):
+        """
+        Method to exit and end the program. Is called
+        when user clicks corresponding exit
+        button on the menu window.
+
+        Parameter:
+            None
+        Returns:
+            None
+        """
+        sys.exit()  # use the exit method from the sys module
+
+    def MenuDisplay(self):
+        # initiate the menu window, and set it's title
+        self.menu = Tk()
+        menu = self.menu
+        menu.title(" Freedge Tracker ")  # TODO do we wanna change the title?
+        # Sets window size
+        menu.geometry("500x750")
+        # Prevent the menu window from being resized
+        menu.resizable(False, False)
+        # Set menu background color
+        menu.configure(bg='black')
+
+        # Button to exit session
+        exit_button = Button(menu, text="            Exit           ", font=("TkDefaultFont", 20), command=self.exit_,
+                             bg="white", width=15)  # initiate the button
+        exit_button.pack(padx=10, pady=10)
+
+        # TODO: the buttons to be added: Create New Database, Update Database, Create Graph (?), ...
+
+        menu.mainloop()
+
+
+if __name__ == '__main__':
+    screen = admin_interface()
+    screen.MenuDisplay()
