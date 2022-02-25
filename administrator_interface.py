5f6fadde845c3b139aa3bf1a7fe1860b4a61992d
kalynkoyanagi <kalynk@uoregon.edu>
2022-02-24 18:25:52 -0800

    Working on the list of data on the menu window

--- administrator_interface.py
+++ administrator_interface.py
@@ -10,6 +10,7 @@ Last Edit By:   Kalyn Koyanagi
 import sys
 from tkinter import *
 from tkinter import filedialog
+from tkinter import ttk
 from freedge_internal_database import database_constants
 from freedge_database import *
 
@@ -53,9 +54,9 @@ class admin_interface:
         # initiate the menu window, and set it's title
         self.menu = Tk()
         menu = self.menu
-        menu.title(" Freedge Tracker ")  # TODO do we wanna change the title?
+        menu.title("Freedge Tracker")  # TODO do we wanna change the title?
         # Sets window size
-        menu.geometry("500x750")
+        menu.geometry("850x650")
         # Prevent the menu window from being resized
         menu.resizable(False, False)
         # Set menu background color
@@ -78,9 +79,28 @@ class admin_interface:
         exit_button.pack(padx=10, pady=10)
 
         # TODO: the buttons to be added: Create Graph (?), ...
+        # Get list of list of freedges. freedges is a list of Freedge instances.
 
+        # TODO: Below is creating the scrollable frame
+
+        treev = ttk.Treeview(menu)
+        treev['columns'] = ('Project Name', 'Location', 'Owner', 'Status', 'Primary Contact')
+        treev.column('#0', width=0, stretch=NO)
+        treev.column('Project Name', anchor=CENTER, width=80)
+        treev.column('Owner', anchor=CENTER, width=80)
+        treev.column('Location', anchor=CENTER, width=80)
+        treev.column('Status', anchor=CENTER, width=80)
+        treev.column('Primary Contact', anchor=CENTER, width=80)
+        treev.heading('#0', text='', anchor=CENTER)
+        treev.heading('Project Name', text='Project Name', anchor=CENTER)
+        treev.heading('Location', text='Location', anchor=CENTER)
+        treev.heading('Owner', text='Owner', anchor=CENTER)
+        treev.heading('Status', text='Status', anchor=CENTER)
+        treev.heading('Primary Contact', text='Primary Contact', anchor=CENTER)
+
+        treev.insert(parent='', index=0, iid=0, text='', values=('Freedge@UO', 'Eugene', 'Ernst', 'Active', 'ernst@freedge.com'))
+        treev.pack()
         menu.mainloop()
-
 
 if __name__ == '__main__':
     screen = admin_interface()
