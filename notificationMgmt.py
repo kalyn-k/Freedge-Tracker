aa57f4e0d2af33d3a90a674065f71e14ee979d1e
liza-richards <lizar@uoregon.edu>
2022-02-24 17:47:28 -0800

    Changed notification management

---
+++ notificationMgmt.py
@@ -0,0 +1,30 @@
+"""
+===============================================================================
+Title:	TODO
+===============================================================================
+Description:	TODO
+
+Authors: 		TODO
+Last Edited: 	2-24-2022
+Last Edit By:	Liza Richards
+"""
+
+import csv
+import string
+
+from freedge_internal_database.database_constants import *
+from caretaker_info_parser import *
+from freedge_data_entry import *
+
+
+# load internal database, get freedge database object, all you have to do to get the information
+# freedge database object.freedges
+
+# have the classes accept the freedge database as an argument,
+
+# expect instructor to send fridge database object or the list of freedges, most likely list of freedges
+
+
+
+
+
