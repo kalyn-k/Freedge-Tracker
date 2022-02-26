989d39e18b48efba555ac0a8341bfa36a2a3f375
kalynkoyanagi <kalynk@uoregon.edu>
2022-02-25 18:33:10 -0800

    Updates to GUI

--- administrator_interface.py
+++ administrator_interface.py
@@ -4,7 +4,7 @@ Title:	Administrator Interface for the Freedge Tracker System
 ===============================================================================
 Description:	TODO
 Authors:        Kalyn Koyanagi
-Last Edited:    2-23-2022
+Last Edited:    2-25-2022
 Last Edit By:   Kalyn Koyanagi
 """
 import sys
@@ -21,21 +21,20 @@ class admin_interface:
         TODO
         """
         # Define user windows for later use
+        self.treev = None
         self.menu = None
 
     def NewDatabase(self, event=None):
         file_path = filedialog.askopenfilename()
         database_path = database_constants.DATABASE_PATH
         new_database_from_csv(database_path, file_path)
+        self.UpdateDisplayData()
 
     def UpdateDatabase(self, event=None):
         file_path = filedialog.askopenfilename()
         database_path = database_constants.DATABASE_PATH
         new_database_from_csv(database_path, file_path)
-
-    def CreateGraph(self):
-        # call build_freedge_graph.py here
-        pass
+        self.UpdateDisplayData()
 
     def exit_(self):
         """
@@ -50,57 +49,73 @@ class admin_interface:
         """
         sys.exit()  # use the exit method from the sys module
 
+    def UpdateDisplayData(self):
+        # Get list of list of freedges. freedges is a list of Freedge instances.
+        # type(fdb) = FreedgeDatabase
+        db_path = database_constants.DATABASE_PATH
+        fdb = load_internal_database(db_path)
+        freedges = fdb.get_freedges()
+        num_db = len(freedges)
+
+        for fridge in range(num_db - 1):
+            self.treev.insert(parent='', index=fridge, iid=fridge, text='', values=(
+                freedges[fridge].project_name, freedges[fridge].fridge_location, freedges[fridge].caretaker_name,
+                freedges[fridge].freedge_status, freedges[fridge].preferred_contact_method))
+
+        self.treev.place(x=340, y=70)
+
     def MenuDisplay(self):
         # initiate the menu window, and set it's title
         self.menu = Tk()
         menu = self.menu
         menu.title("Freedge Tracker")  # TODO do we wanna change the title?
         # Sets window size
-        menu.geometry("850x650")
+        menu.geometry("875x500")
         # Prevent the menu window from being resized
         menu.resizable(False, False)
         # Set menu background color
         menu.configure(bg='black')
 
+        # Create a Header for the menu
+        header = Label(menu, width=50, text="Freedge Tracker", bg="black", fg="white",
+                       font=("TkDefaultFont", 22))
+        header.pack(side=TOP, padx=10, pady=15)
+
         # Button to Create new database (db)
         create_db_button = Button(menu, text="Create new Database", font=("TkDefaultFont", 20),
                                   command=self.NewDatabase,
                                   bg="white", width=20)  # initiate the button
-        create_db_button.pack(padx=10, pady=10)
+        create_db_button.place(x=45, y=70)
 
         # Button to update database
         update_db_button = Button(menu, text="Update Database", font=("TkDefaultFont", 20), command=self.UpdateDatabase,
                                   bg="white", width=20)  # initiate the button
-        update_db_button.pack(padx=10, pady=10)
+        update_db_button.place(x=45, y=120)
 
         # Button to exit session
         exit_button = Button(menu, text="            Exit           ", font=("TkDefaultFont", 20), command=self.exit_,
                              bg="white", width=20)  # initiate the button
-        exit_button.pack(padx=10, pady=10)
+        exit_button.place(x=45, y=170)
 
-        # TODO: the buttons to be added: Create Graph (?), ...
-        # Get list of list of freedges. freedges is a list of Freedge instances.
+        self.treev = ttk.Treeview(menu, height=20)
 
-        # TODO: Below is creating the scrollable frame
+        self.treev['columns'] = ('Project Name', 'Location', 'Owner', 'Status', 'Primary Contact')
+        self.treev.column('#0', width=0, stretch=NO)
+        self.treev.column('Project Name', anchor=CENTER, width=125)
+        self.treev.column('Owner', anchor=CENTER, width=90)
+        self.treev.column('Location', anchor=CENTER, width=90)
+        self.treev.column('Status', anchor=CENTER, width=90)
+        self.treev.column('Primary Contact', anchor=CENTER, width=110)
+        self.treev.heading('#0', text='', anchor=CENTER)
+        self.treev.heading('Project Name', text='Project Name', anchor=CENTER)
+        self.treev.heading('Location', text='Location', anchor=CENTER)
+        self.treev.heading('Owner', text='Owner', anchor=CENTER)
+        self.treev.heading('Status', text='Status', anchor=CENTER)
+        self.treev.heading('Primary Contact', text='Primary Contact', anchor=CENTER)
 
-        treev = ttk.Treeview(menu)
-        treev['columns'] = ('Project Name', 'Location', 'Owner', 'Status', 'Primary Contact')
-        treev.column('#0', width=0, stretch=NO)
-        treev.column('Project Name', anchor=CENTER, width=80)
-        treev.column('Owner', anchor=CENTER, width=80)
-        treev.column('Location', anchor=CENTER, width=80)
-        treev.column('Status', anchor=CENTER, width=80)
-        treev.column('Primary Contact', anchor=CENTER, width=80)
-        treev.heading('#0', text='', anchor=CENTER)
-        treev.heading('Project Name', text='Project Name', anchor=CENTER)
-        treev.heading('Location', text='Location', anchor=CENTER)
-        treev.heading('Owner', text='Owner', anchor=CENTER)
-        treev.heading('Status', text='Status', anchor=CENTER)
-        treev.heading('Primary Contact', text='Primary Contact', anchor=CENTER)
-
-        treev.insert(parent='', index=0, iid=0, text='', values=('Freedge@UO', 'Eugene', 'Ernst', 'Active', 'ernst@freedge.com'))
-        treev.pack()
+        self.treev.place(x=340, y=70)
         menu.mainloop()
+
 
 if __name__ == '__main__':
     screen = admin_interface()
